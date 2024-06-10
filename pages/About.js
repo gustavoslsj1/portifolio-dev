ad3a740c0932958299d4b7285044728d6743608b
gustavo lima sales junior <102685940+gustavoslsj1@users.noreply.github.com>
2024-06-09 22:32:35 -0300

    css

--- pages/About.js
+++ pages/About.js
@@ -14,17 +14,17 @@ function About() {
                     <div className={styles.web} >
                         <img src="https://cdn-icons-png.freepik.com/256/3914/3914588.png?semt=ais_hybrid"  ></img>
                         <h3>web developer</h3>
-                        <p className="paragrafo">front e backand developer com  mais de 3 projetos pessoais. 1 ano e meio de experiência na area </p>
+                        <p className={styles.paragrafo}>front e backand developer com  mais de 3 projetos pessoais. 1 ano e meio de experiência na area </p>
                     </div>
                     <div className={styles.mobile}>
                         <img src="https://cdn-icons-png.freepik.com/256/3841/3841536.png?semt=ais_hybrid"></img>
                         <h3>mobile developer</h3>
-                        <p className="paragrafo" >front developer com 1 projeto feito em react native. menos de 1 ano de experiência  </p>
+                        <p className={styles.paragrafo} >front developer com 1 projeto feito em react native. menos de 1 ano de experiência  </p>
                     </div>
                     <div className={styles.gamer} >
                         <img src="https://cdn-icons-png.freepik.com/256/15399/15399467.png?semt=ais_hybrid" alt="as"></img>
                         <h3>gamer developer</h3>
-                        <p className="paragrafo"> area de hobbie com 1 jogo simples feito na unreal engine motor de desenvolvimento de jogos,
+                        <p className={styles.paragrafo}> area de hobbie com 1 jogo simples feito na unreal engine motor de desenvolvimento de jogos,
                             com 1 curso feito na udemy
                         </p>
                     </div>
